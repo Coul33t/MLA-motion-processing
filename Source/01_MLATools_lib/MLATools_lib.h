6fc5ac1dd07651446690909a0d6b8965fbf53300
Coul33t <Coulis1990@gmail.com>
2017-03-06 17:32:50 +0100

    Removed unused classes (Graph and Tree)

--- Source/01_MLATools_lib/MLATools_lib.h
+++ Source/01_MLATools_lib/MLATools_lib.h
@@ -1,9 +1,7 @@
 #ifndef __MLA_TOOLS_LIB_INCLUDE_FILES__ 
 #define __MLA_TOOLS_LIB_INCLUDE_FILES__
 
-
-#include "MLAGraph/MLAGraph.h"
+#include "MLACommonInclude.h"
 #include "MLAShader/MLAShader.h"
-#include "MLATree/MLATree.h"
 
 #endif //__MLA_TOOLS_LIB_INCLUDE_FILES__
