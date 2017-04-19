b91336962e7e929d51c7acf73abb334df0123186
Coul33t <Coulis1990@gmail.com>
2017-04-19 17:24:11 +0200

    Added Neuron support

---
+++ Source/05_MLANeuron_lib/MLANeuron_lib.h
@@ -0,0 +1,6 @@
+#ifndef __MLA_NEURON_LIB_INCLUDE_FILES__ 
+#define __MLA_NEURON_LIB_INCLUDE_FILES__  
+
+#include "MLANeuronConnection\NeuronConnection.h"
+
+#endif //__MLA_NEURON_LIB_INCLUDE_FILES__
