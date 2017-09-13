e6fcb4c30016a08255d62506ccf188de5e6804e1
Coul33t <Coulis1990@gmail.com>
2017-09-13 10:45:45 +0200

    Added PipeExport (unimplemented)

---
+++ Source/01_MLATools_lib/MLADataExport/PipeExport.h
@@ -0,0 +1,21 @@
+/**
+Class for creating pipe
+PipeExport.h
+
+@author Quentin Couland
+@version 0.1
+
+*/
+
+#ifndef __MLA_PIPEEXPORT_H__
+#define __MLA_PIPEEXPORT_H__
+
+class PipeExport {
+public:
+    PipeExport();
+    ~PipeExport();
+
+private:
+
+};
+#endif
