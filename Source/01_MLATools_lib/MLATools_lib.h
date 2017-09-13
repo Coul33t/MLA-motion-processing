8eddb785c348fa2e8cffec08dbd336ae76ca0f5b
Coul33t <Coulis1990@gmail.com>
2017-09-13 10:48:03 +0200

    Added PipeExport (unimplemented)

--- Source/01_MLATools_lib/MLATools_lib.h
+++ Source/01_MLATools_lib/MLATools_lib.h
@@ -5,5 +5,6 @@
 #include "MLAShader/MLAShader.h"
 #include "MLAMotionOperation/MLAMotionOperation.h"
 #include "MLADataExport/CSVExport.h"
+#include "MLADataExport/PipeExport.h"
 
 #endif //__MLA_TOOLS_LIB_INCLUDE_FILES__
