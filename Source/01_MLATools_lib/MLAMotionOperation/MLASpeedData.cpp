45bf1afe2bf09f4f1e464b4a8a6539761ec5804d
Coul33t <Coulis1990@gmail.com>
2017-11-20 11:38:06 +0100

    Rewrote some things

--- Source/01_MLATools_lib/MLAMotionOperation/MLASpeedData.cpp
+++ Source/01_MLATools_lib/MLAMotionOperation/MLASpeedData.cpp
@@ -1,24 +1,31 @@
 #include "MLASpeedData.h"
 
-//constructeurs
-MLASpeedData::MLASpeedData(const double& intervalNumber, const double& timeStepInterFrame, const double& nbframe)
-	: m_intervalTime((nbframe *timeStepInterFrame) / static_cast<double>(intervalNumber)) {
+SpeedData::SpeedData(const double& intervalNumber, const double& timeStepInterFrame, const double& nbframe)
+	: m_intervalTime((nbframe * timeStepInterFrame) / static_cast<double>(intervalNumber)) {
 
 }
 
-MLASpeedData::~MLASpeedData() {
+SpeedData::~SpeedData() {
 
 }
 
-//accesseurs
-bool MLASpeedData::getJointSpeedAndTime(const std::string& jointName, const unsigned int& index, const double* speedValue, const double* timeValue) const {
+
+/** Returns a couple of speed and time.
+
+	@param jointName the name of the joint
+	@param index the index of the frame
+	@param speedTime the returned values of speed and time
 	
+	@return bool whenever the values for the index and the joint were found or not
+*/
+bool SpeedData::getJointSpeedAndTime(const std::string& jointName, const unsigned int index, std::pair<double, double>& speedTime) const {
+
 	if (index < m_speedData.size()) {
 		std::map<std::string, double>::const_iterator iter = m_speedData[index].m_speedSet.find(jointName);
 
 		if (iter != m_speedData[index].m_speedSet.end()) {
-			speedValue = &iter->second;
-			timeValue = &m_speedData[index].m_time;
+			speedTime.first = iter->second;
+			speedTime.second = m_speedData[index].m_time;
 			return true;
 		}
 	}
@@ -26,13 +33,20 @@ bool MLASpeedData::getJointSpeedAndTime(const std::string& jointName, const unsi
 	return false;
 }
 
-//accesseurs
-bool MLASpeedData::getJointSpeedAndTimeVector(const std::string& jointName, std::vector<std::pair<double, double>>& speedAndTimeVector) const {
+/** Returns a vector of couples of speed and time.
+
+@param jointName the name of the joint
+@param index the index of the frame
+@param a pair of speed and time
+
+@return bool whenever the joint has been found and/or if it's vector is empty
+*/
+bool SpeedData::getJointSpeedAndTimeVector(const std::string& jointName, std::vector<std::pair<double, double>>& speedAndTimeVector) const {
 	
 	speedAndTimeVector.clear();
 
 	// IF the vector is empty OR the joint is not found in the map
-	if (m_speedData.empty() || m_speedData[0].m_speedSet.find(jointName) == m_speedData[0].m_speedSet.end())
+	if (m_speedData.empty() || m_speedData[0].m_speedSet.count(jointName) == 0)
 		return false;
 	
 	for (unsigned int i = 0; i < m_speedData.size(); ++i) {
@@ -43,20 +57,20 @@ bool MLASpeedData::getJointSpeedAndTimeVector(const std::string& jointName, std:
 }
 
 
-unsigned int MLASpeedData::getNbInterval() const { 
+unsigned int SpeedData::getNbInterval() const { 
 	return m_speedData.size(); 
 }
 
-const double& MLASpeedData::getIntervalTime() const { 
+const double SpeedData::getIntervalTime() const { 
 	return m_intervalTime; 
 }
 
-double MLASpeedData::getDuration() const { 
-	return (double)(getNbInterval()) * m_intervalTime; 
+double SpeedData::getDuration() const { 
+	return (double)(m_speedData.size()) * m_intervalTime;
 }
 
-//modificateurs
-void MLASpeedData::addFrameSpeed(const std::map<std::string, double>& bodySpeed) {
+
+void SpeedData::addFrameSpeed(const std::map<std::string, double>& bodySpeed) {
 	MLABodySpeed newbodySpeed;
 	newbodySpeed.m_speedSet = bodySpeed;
 	newbodySpeed.m_time = getNbInterval() * m_intervalTime;
--- Source/01_MLATools_lib/MLAMotionOperation/MLASpeedData.h
+++ Source/01_MLATools_lib/MLAMotionOperation/MLASpeedData.h
@@ -3,29 +3,28 @@
 #include <vector>
 #include <string>
 
-class MLASpeedData {
-
-	private:
-		struct MLABodySpeed {
-			//temps auquel les vitesse des articulations prennent leur valeur (?)
-			double m_time; 
-			// Set of speed values for each joint at time m_time
-			std::map<std::string, double>  m_speedSet; 
-		};
+class SpeedData {
 
 	public:
-		MLASpeedData(const double& intervalNumber, const double& timeStepInterFrame, const double& nbframe);
-		~MLASpeedData();
+		SpeedData(const double& intervalNumber, const double& timeStepInterFrame, const double& nbframe);
+		~SpeedData();
 
-		bool getJointSpeedAndTime(const std::string& jointName, const unsigned int& index, const double* speedValue, const double* timeValue) const;
+		bool getJointSpeedAndTime(const std::string& jointName, const unsigned int index, std::pair<double, double>& speedTime) const;
 		bool getJointSpeedAndTimeVector(const std::string& jointName, std::vector<std::pair<double,double>>& speedAndTimeVector) const;
 		unsigned int getNbInterval() const;
-		const double& getIntervalTime() const;
+		const double getIntervalTime() const;
 		double getDuration() const;
 
 		void addFrameSpeed(const std::map<std::string, double>& bodySpeed);
 
 	private:
+		struct MLABodySpeed {
+			// Time at which the values are computed
+			double m_time;
+			// Set of speed values for each joint at time m_time
+			std::map<std::string, double> m_speedSet;
+		};
+
 		// Set of speed and corresponding time, for each joint for the whole motion
 		std::vector<MLABodySpeed> m_speedData; 
 		// Total animation time
