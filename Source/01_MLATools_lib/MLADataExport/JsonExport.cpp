ff8a1fc49c7dd4ed2f9ce0cc069398a4a7239608
Coul33t <Coulis1990@gmail.com>
2018-02-15 09:30:16 +0100

    Used the json library instead of handmade json

--- Source/01_MLATools_lib/MLADataExport/JsonExport.cpp
+++ Source/01_MLATools_lib/MLADataExport/JsonExport.cpp
@@ -50,7 +50,6 @@ namespace Mla {
 			return true;	
 		}
 
-		//TODO: redo with json library
 		bool ExportMotionSegmentationInformations(const SegmentationInformation& seg_motion_info, const std::string& folder_name, const std::string& file_name) {
 			
 			if (!(CreateDirectory(("..\\..\\..\\Data\\Speed\\" + folder_name).c_str(), NULL) || GetLastError() == ERROR_ALREADY_EXISTS)) {
@@ -61,11 +60,11 @@ namespace Mla {
 			json json_file;
 			json_file["desired left cut number"] = seg_motion_info.left_cut;
 			json_file["desired right cut number"] = seg_motion_info.right_cut;
-			json_file["final frame number"] = seg_motion_info.final_frame_number;
+			json_file["final frames count"] = seg_motion_info.final_frame_number;
 			json_file["final interframe time"] = seg_motion_info.final_interframe_time;
 			json_file["savgol"]["window size"] = seg_motion_info.savgol_window_size;
 			json_file["savgol"]["polynom order"] = seg_motion_info.savgol_polynom_order;
-			//TODO: add joint list
+			json_file["throw duration"] = (seg_motion_info.throw_idx.second - seg_motion_info.throw_idx.first) * seg_motion_info.final_interframe_time;
 
 			std::ofstream outfile;
 
@@ -85,38 +84,35 @@ namespace Mla {
 			return true;
 		}
 
-		//TODO: redo with json library
-		bool ExportMotionInformations(const MotionInformation& motion_info, const std::string& folder_name, const std::string& file_name) {
-			if (CreateDirectory(("..\\..\\..\\Data\\Speed\\" + folder_name).c_str(), NULL) || GetLastError() == ERROR_ALREADY_EXISTS) {
-				std::ofstream outfile;
-
-				// Wipe any pre-existing file.
-				outfile.open("../../../Data/Speed/" + folder_name + "/" + file_name + ".json", std::ios::out);
-				outfile.close();
-
-				outfile.open("../../../Data/Speed/" + folder_name + "/" + file_name + ".json", std::ios::out | std::ios::app);
-
-				if (!outfile.fail()) {
-					outfile << "{\n";
-					outfile << "\t\"motion name\": \"" << motion_info.motion_name << "\",\n";
-					outfile << "\t\"frame number\": " << motion_info.frame_number << ",\n";
-					outfile << "\t\"interframe time\": " << motion_info.frame_time << ",\n";
-					outfile << "\t\"root name\": \"" << motion_info.root_name << "\"\n";
-					outfile << "}";
-					outfile.close();
-					return true;
-				}
-
-				else {
-					std::cout << "Failed to open file " + file_name + ".json" << std::endl;
-					return false;
-				}
-			}
-
-			else {
+		bool ExportMotionInformations(const MotionInformation& motion_info, std::vector<std::string> joints_names, const std::string& folder_name, const std::string& file_name) {
+			if (!(CreateDirectory(("..\\..\\..\\Data\\Speed\\" + folder_name).c_str(), NULL) || GetLastError() == ERROR_ALREADY_EXISTS)) {
 				std::cout << "Failed to create the folder " + folder_name << std::endl;
 				return false;
 			}
+
+			json json_file;
+			json_file["motion name"] = motion_info.motion_name;
+			json_file["frames count"] = motion_info.frame_number;
+			json_file["interframe time"] = motion_info.frame_time;
+			json_file["root name"] = motion_info.root_name;
+			json_file["joints names"] = joints_names;
+
+			std::ofstream outfile;
+
+			// Wipe any pre-existing file.
+			outfile.open("../../../Data/Speed/" + folder_name + "/" + file_name + ".json", std::ios::out);
+			outfile.close();
+
+			outfile.open("../../../Data/Speed/" + folder_name + "/" + file_name + ".json", std::ios::out | std::ios::app);
+
+			if (outfile.fail()) {
+				std::cout << "Failed to open file " + file_name + ".json" << std::endl;
+				return false;
+			}
+
+			outfile << json_file;
+			outfile.close();
+			return true;
 		}
 	}
 }
--- Source/01_MLATools_lib/MLADataExport/JsonExport.h
+++ Source/01_MLATools_lib/MLADataExport/JsonExport.h
@@ -20,7 +20,7 @@ namespace Mla {
 	namespace JsonExport {
 		bool ExportData(Data& data, const std::string& = "default_folder_name", const std::string& = "default_subfolder_name", const std::string& = "default_file_name");
 		bool ExportMotionSegmentationInformations(const SegmentationInformation& seg_motion_info, const std::string& folder_name, const std::string& file_name);
-		bool ExportMotionInformations(const MotionInformation& motion_info, const std::string& folder_name, const std::string& file_name);
+		bool ExportMotionInformations(const MotionInformation& motion_info, std::vector<std::string>, const std::string& folder_name, const std::string& file_name);
 	}
 }
 #endif //__MLA_JSON_EXPORT_H__
