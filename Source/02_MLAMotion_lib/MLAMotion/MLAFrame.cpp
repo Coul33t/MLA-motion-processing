fd7539dbfa13d6305062cf762684501a66ba02b7
Quentin <Coulis1990@gmail.com>
2019-02-12 17:43:01 +0100

    Added documentation

--- Source/02_MLAMotion_lib/MLAMotion/MLAFrame.cpp
+++ Source/02_MLAMotion_lib/MLAMotion/MLAFrame.cpp
@@ -1,9 +1,21 @@
+/**
+* \file MLAFrame.cpp
+* \brief This file contains the Frame class, representing a single frame for a motion.
+* \author Quentin.C
+* \version 0.1
+* \date 12 February 2019
+*/
+
 #include "MLAFrame.h"
 
+/** Constructor
+*/
 Frame::Frame() {
 	m_root = nullptr;
 }
 
+/** Destructor
+*/
 Frame::~Frame() {
 	// No need to delete m_root since it will be deleted by this
 	if (!m_joints.empty())
@@ -11,10 +23,17 @@ Frame::~Frame() {
 			delete (m_joints[i]);
 }
 
+// Error? Need a deep copy of the other frame m_joints?
+/**
+* @param frame The frame to copy
+*/
 Frame::Frame(const Frame& frame) {
 	m_joints = frame.m_joints;
 }
 
+/** Equal operator overload
+* @param frame The frame to copy
+*/
 Frame& Frame::operator=(const Frame& frame) {
 	m_joints = frame.m_joints;
 	return *this;
@@ -40,6 +59,9 @@ void Frame::setRoot(Joint* root) {
 	m_root = root;
 }
 
+/** Returns a vector containing all of the frame joints' names.
+* @return A vector of joints' names for the current frame
+*/
 std::vector<std::string> Frame::getJointsName() const {
 	std::vector<std::string> joint_names;
 
@@ -50,6 +72,10 @@ std::vector<std::string> Frame::getJointsName() const {
 	return joint_names;
 }
 
+/** Returns a joint data from its name.
+* @param joint_name Name of the desired joint
+* @return The joint if it was found, else a nullptr
+*/
 Joint* Frame::getJoint(const std::string& joint_name) {
 	auto it = find_if(m_joints.begin(), m_joints.end(), [&joint_name](const Joint* obj) {return obj->getName() == joint_name; });
 	if (it != m_joints.end()) {
@@ -60,6 +86,10 @@ Joint* Frame::getJoint(const std::string& joint_name) {
 	}
 }
 
+/** Returns a joint data from its index.
+* @param idx Index of the desired joint
+* @return The joint if the index is inferior to the joints' vector size, else a nullptr
+*/
 Joint* Frame::getJoint(const unsigned int idx) {
 	if(idx < m_joints.size()) {
 		return m_joints[idx];
@@ -69,6 +99,9 @@ Joint* Frame::getJoint(const unsigned int idx) {
 	}
 }
 
+/** Duplicates the current frame.
+* @return A copy of the current frame
+*/
 Frame* Frame::duplicateFrame() {
 	Frame* copied_frame = new Frame();
 
